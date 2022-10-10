2f84c92ad5f86babf792e81bb5728d612a61fad5
jemelsalby <jemelsalby3@gmail.com>
2022-10-11 00:09:17 +0530

    shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
+
+import { AccessComponent } from './access/access.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+   
+    AccessComponent
   ],
   imports: [
     BrowserModule,
